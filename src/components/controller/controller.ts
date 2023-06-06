b78bb56929388b6e2ae8133a5fd5fb1665d101f4
Ihor Serhiienko <igorserhiyenko@gmail.com>
2023-06-06 17:46:17 +0300

    refactor: add types to controller

--- src/components/controller/controller.ts
+++ src/components/controller/controller.ts
@@ -1,7 +1,22 @@
 import AppLoader from './appLoader';
+import { SourcesItem } from '../view/sources/sources';
+import { NewsItem } from '../view/news/news';
+
+interface SourcesData {
+    status: string;
+    totalResults: number;
+    sources: SourcesItem[];
+}
+
+interface ArticleData {
+    status: string;
+    articles: NewsItem[];
+}
+
+type controllerFunc<T> = (data?: T) => void;
 
 class AppController extends AppLoader {
-    getSources(callback) {
+    getSources(callback: controllerFunc<SourcesData>) {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -10,14 +25,14 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
-        let target: HTMLButtonElement | null = e.target;
+    getNews(e: Event, callback: controllerFunc<ArticleData>) {
+        let target = <HTMLElement>e.target;
         const newsContainer = <HTMLElement>e.currentTarget;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
                 const sourceId = target.getAttribute('data-source-id');
-                if (newsContainer.getAttribute('data-source') !== sourceId) {
+                if (newsContainer.getAttribute('data-source') !== sourceId && sourceId) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
                         {
@@ -31,7 +46,7 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode;
+            target = <HTMLElement>target.parentNode;
         }
     }
 }
