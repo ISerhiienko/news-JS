55df4b4fe10ce8d7564df4a78341a9a26bfbaf44
Ihor Serhiienko <igorserhiyenko@gmail.com>
2023-06-06 14:08:13 +0300

    feat: set up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
 
 const baseConfig = {
     entry: path.resolve(__dirname, './src/index.js'),
@@ -9,19 +10,32 @@ const baseConfig = {
     module: {
         rules: [
             {
+                test: /\.ts$/i,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jp(e*)g|gif|svg)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: '[name][ext]',
+                },
             },
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.ts', '.js'],
     },
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, '../dist'),
     },
     plugins: [
+        new EslingPlugin({ extensions: 'ts' }),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
             filename: 'index.html',
