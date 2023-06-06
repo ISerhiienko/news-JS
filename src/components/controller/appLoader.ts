478dfb97a18ea5aa87d8fb6f8405bfcb1629ffaf
Ihor Serhiienko <igorserhiyenko@gmail.com>
2023-06-06 18:10:44 +0300

    feat: add apiKey

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '38ad1c60fe91441e91329ef0ee182771', // получите свой ключ https://newsapi.org/
         });
     }
 }
