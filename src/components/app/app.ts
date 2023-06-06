4a5c5ff76e6219ba60e8f0e57ac19e368475ebeb
Ihor Serhiienko <igorserhiyenko@gmail.com>
2023-06-06 18:09:30 +0300

    refactor: add types for loader

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,23 +1,28 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
-import { NewsItem } from '../view/news/news';
-import { SourcesItem } from '../view/sources/sources';
 
 class App {
+    controller: AppController;
+    view: AppView;
+
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
-    start(): void {
-        const sourcesElement: HTMLElement | null = document.querySelector('.sources');
-
-        if (sourcesElement) {
-            sourcesElement.addEventListener('click', (e: Event) =>
-                this.controller.getNews(e, (data: NewsItem[]) => this.view.drawNews(data))
-            );
-        }
-        this.controller.getSources((data: SourcesItem[]) => this.view.drawSources(data));
+    start() {
+        (<HTMLElement>document.querySelector('.sources')).addEventListener('click', (e) =>
+            this.controller.getNews(e, (data) => {
+                if (data) {
+                    this.view.drawNews(data);
+                }
+            })
+        );
+        this.controller.getSources((data) => {
+            if (data) {
+                this.view.drawSources(data);
+            }
+        });
     }
 }
 
--- src/components/controller/loader.ts
+++ src/components/controller/loader.ts
@@ -1,11 +1,18 @@
+import { NewsItem } from '../view/news/news';
+
+type controllerFunc<T> = (data?: T) => void;
+
 class Loader {
-    constructor(baseLink, options) {
+    baseLink: string;
+    options: Record<string, string>;
+
+    constructor(baseLink: string, options: Record<string, string>) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
     getResp(
-        { endpoint, options = {} },
+        { endpoint = '', options = {} },
         callback = () => {
             console.error('No callback for GET response');
         }
@@ -13,7 +20,7 @@ class Loader {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    errorHandler(res: Response) {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,7 +30,7 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
+    makeUrl(options: Record<string, string>, endpoint: string) {
         const urlOptions = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
@@ -34,7 +41,7 @@ class Loader {
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    load(method: string, endpoint: string, callback: controllerFunc<NewsItem>, options = {}) {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
